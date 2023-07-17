c1a65d0e9a7ae582bd54ccee46a73539bc3aece4
MDMwakulo <mdmwakulo@gmail.com>
2023-07-17 16:52:02 +0300

    Numeric Separators

--- script.js
+++ script.js
@@ -289,6 +289,7 @@ console.log(Number.isInteger(23.55)); // false
 console.log(Number.isInteger(23 / 0)); // false
 */
 
+/*
 // Math and Rounding
 console.log(Math.sqrt(25)); // 5
 console.log(25 ** (1 / 2)); // 5
@@ -334,3 +335,47 @@ console.log((2.7).toFixed(0)); // 3
 console.log((2.7).toFixed(3)); // 2.700
 console.log((2.345).toFixed(2)); // 2.700
 console.log(+(2.345).toFixed(2)); // 2.700
+*/
+
+/*
+// The Remainder Opeartor
+console.log(5 % 2);
+console.log(5 / 2); // 5 = 2 * 2 + 1
+
+console.log(8 % 3);
+console.log(8 / 3); // 8 = 3 * 2 + 2
+
+console.log(6 % 2);
+console.log(6 / 2);
+
+console.log(7 % 2);
+console.log(7 / 2);
+
+const isEven = n => n % 2 === 0;
+console.log(isEven(8));
+console.log(isEven(23));
+console.log(isEven(514));
+
+labelBalance.addEventListener('click', function () {
+  [...document.querySelectorAll('.movements__row')].forEach((row, i) => {
+    if (i % 2 === 0) row.style.backgroundColor = 'orangered';
+    if (i % 3 === 0) row.style.backgroundColor = 'blue';
+  });
+});
+*/
+
+// 287,460,000,000
+const diameter = 287_460_000_000;
+console.log(diameter);
+
+const price = 345_99;
+console.log(price);
+
+const transferFee1 = 15_00;
+const transferFee2 = 1_500;
+
+const PI = 3.14_15; //ERROR
+console.log(PI);
+
+console.log(Number('230_000')); // ERROR
+console.log(parseInt('230_000')); // 230
